2972de55d9423931bcd70057f5d0e9ecbc6bc8a8
endlessashley <81969558+endlessashley@users.noreply.github.com>
2021-10-12 10:11:51 -0400

    mongo atlas connection

--- server.js
+++ server.js
@@ -14,10 +14,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// mongoose.connect("mongodb://localhost/budget", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
+
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/budget',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 app.use(require("./routes/api.js"));
 app.listen(PORT, () => {
